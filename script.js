650aeca0b1c89730900ddf313ad10019acb8f40d
derarabelachew <109346634+derarabelachew@users.noreply.github.com>
2023-05-24 00:42:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const button = document.getElementById('colorButton');
+let currentColorScheme = 1;
+
+button.addEventListener('click', changeColorScheme);
+
+function changeColorScheme() {
+  currentColorScheme++;
+  if (currentColorScheme > 3) {
+    currentColorScheme = 1;
+  }
+
+  body.className = `color-scheme-${currentColorScheme}`;
+}
+function openGitHubProject1() {
+  window.location.href = 'https://github.com/users/derarabelachew/projects/1/views/1';
+}
+
